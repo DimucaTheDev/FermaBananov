0a806a3ac1f26019da6b7e9cb9055e6cbc392467
DimucaTheDev <74795935+DimucaTheDev@users.noreply.github.com>
2024-06-20 20:33:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FermaBananov
+{
+    internal class Program
+    {
+        [DllImport("user32", EntryPoint = "ShowWindow")]
+        static extern void Minimize(IntPtr hWnd, int state = 6); //6 - SW_MINIMIZE
+        static void Main(string[] args)
+        {
+            DateTime latest = DateTime.Now;
+            var url = "steam://rungameid/2923300/";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            new Thread(s =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Title =
+                            $"Next start in {(latest + new TimeSpan(3, 0, 0) - DateTime.Now).ToString("g")}";
+                    }
+                    catch { } //if console window is unavailable by some reason
+                }
+            }).Start();
+            while (true)
+            {
+                Console.Write($"{(latest = DateTime.Now).ToString("g")} Starting...   ");
+                Process.Start(psi);
+                Thread.Sleep(5000);
+                var process = Process.GetProcessesByName("banana")[0];
+                Minimize(process!.MainWindowHandle);
+                Thread.Sleep(60 * 1000);
+                process.Kill();
+                Console.WriteLine("Killed");
+                Thread.Sleep(1000 * 60 * 60 * 3);
+            }
+        }
+    }
+}
